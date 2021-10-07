99ddec702b862a2f746997f38f07b5ff0265fead
prashanth9292 <prasantpurimitla@gmail.com>
2021-10-08 00:56:00 +0530

    first commit

---
+++ MyWpfDependency/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWpfDependency
+{
+    public class Logger
+    {
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+    }
+}
